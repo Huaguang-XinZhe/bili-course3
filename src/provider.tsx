eb80cb22fc0e91fe1b3c33326637d320088070e6
Huaguang <2475096613@qq.com>
2025-05-05 14:28:41 +0800

    chore: 删除一些无关文件

--- src/provider.tsx
+++
@@ -1,6 +0,0 @@
-import type { NavigateOptions } from "react-router-dom";
-import { useHref, useNavigate } from "react-router-dom";
-
-export function Provider({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
